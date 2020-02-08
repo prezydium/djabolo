61938c1ee70008979558b3e41b2146050c66df29
Krzysztof Syska <prezydium834@gmail.com>
2020-02-08 14:36:02 +0100

    First hit added

--- src/main/java/sda/krzysztof/djabolo/App.java
+++ src/main/java/sda/krzysztof/djabolo/App.java
@@ -1,13 +1,21 @@
 package sda.krzysztof.djabolo;
 
 
+import sda.krzysztof.djabolo.actor.Monster;
 import sda.krzysztof.djabolo.actor.Player;
+
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Dungeon master says: Hello World!");
         Player player = PlayerCreator.createPlayer();
-        System.out.println(player);
+        List<Monster> monsters = MonsterCreationUtil.createMonsters();
+
+        monsters.get(0).receiveDamage(player.attack());
+        monsters.stream().filter(m -> m.isAlive())
+                .forEach(singleMonster -> player.receiveDamage(singleMonster.attack()));
+        System.out.println(monsters);
     }
 
 
--- src/main/java/sda/krzysztof/djabolo/actor/Creature.java
+++ src/main/java/sda/krzysztof/djabolo/actor/Creature.java
@@ -9,4 +9,42 @@ public abstract class Creature {
     protected boolean alive;
     protected WeaponType weaponType;
 
+    public void receiveDamage(int dmg) {
+        hp -= dmg;
+        if (hp <= 0){
+            alive = false;
+        }
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(WeaponType weaponType) {
+        this.weaponType = weaponType;
+    }
 }
